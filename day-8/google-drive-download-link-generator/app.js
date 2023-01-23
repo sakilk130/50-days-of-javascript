fc7fe15cabc92693769f5820c5cc13e086ca7842
sakilk130 <sakilk130@gmail.com>
2023-01-24 00:35:43 +0600

    style: day-8 / design complete

--- day-8/google-drive-download-link-generator/app.js
+++ day-8/google-drive-download-link-generator/app.js
@@ -1,6 +1,8 @@
 const pasteLink = document.getElementById('paste-link');
 const pasteLinkSection = document.querySelector('.paste-link-section');
 const radioBtn = document.querySelectorAll('input[name="tab"]');
+const embedAudioSection = document.querySelector('.embed-audio-section');
+const embedVideoSection = document.querySelector('.embed-video-section');
 
 if (pasteLink.checked) {
   pasteLinkSection.style.display = 'flex';
@@ -10,8 +12,16 @@ radioBtn.forEach((btn) => {
   btn.addEventListener('change', () => {
     if (btn.id === 'paste-link') {
       pasteLinkSection.style.display = 'flex';
+      embedAudioSection.style.display = 'none';
+      embedVideoSection.style.display = 'none';
+    } else if (btn.id === 'embed-audio') {
+      pasteLinkSection.style.display = 'none';
+      embedAudioSection.style.display = 'flex';
+      embedVideoSection.style.display = 'none';
     } else {
       pasteLinkSection.style.display = 'none';
+      embedAudioSection.style.display = 'none';
+      embedVideoSection.style.display = 'flex';
     }
   });
 });
