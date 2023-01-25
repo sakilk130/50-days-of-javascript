3172db8844eb9481c668ed22e7ea062b84b34d18
sakilk130 <sakilk130@gmail.com>
2023-01-25 20:56:32 +0600

    feat: day-8 / complete google drive download link generate

--- day-8/google-drive-download-link-generator/app.js
+++ day-8/google-drive-download-link-generator/app.js
@@ -3,6 +3,14 @@ const pasteLinkSection = document.querySelector('.paste-link-section');
 const radioBtn = document.querySelectorAll('input[name="tab"]');
 const embedAudioSection = document.querySelector('.embed-audio-section');
 const embedVideoSection = document.querySelector('.embed-video-section');
+const gdriveLink = document.getElementById('gdrive-link');
+const generateBtn = document.getElementById('btn');
+const directLink = document.getElementById('direct-download-link');
+const copyBtn = document.querySelector('.copy');
+const embedAudio = document.querySelector('#embed-audio-link');
+const embedAudioCopyBtn = document.querySelector('#embed-audio-copy');
+const embedVideo = document.getElementById('embed-video-link');
+const embedVideoCopyBtn = document.querySelector('#embed-video-copy');
 
 if (pasteLink.checked) {
   pasteLinkSection.style.display = 'flex';
@@ -24,4 +32,52 @@ radioBtn.forEach((btn) => {
       embedVideoSection.style.display = 'flex';
     }
   });
+});
+
+generateBtn.addEventListener('click', () => {
+  const link = gdriveLink.value;
+  const isValid = link.includes('https://drive.google.com/file/d/');
+  if (!isValid) {
+    alert('Please enter a valid Google Drive link');
+  }
+  const getDirectLink = gdriveLink.value
+    .replace(
+      'https://drive.google.com/file/d/',
+      'https://drive.google.com/uc?export=download&id='
+    )
+    .replace('/view?usp=sharing', '');
+  directLink.value = getDirectLink;
+
+  // Embed Audio
+  const audio1 = '<audio width="300" height="32" controls="controls" src="';
+  const audio2 = '" type="audio/mp3"></audio>';
+  const embedAudioLink = audio1 + getDirectLink + audio2;
+  embedAudio.value = embedAudioLink;
+
+  // Embed Video
+  const getVideoLink = gdriveLink.value.replace('/view?usp=sharing', '');
+  const video1 = '<iframe src="';
+  const video2 = '/preview" width="560" height="315"></iframe>';
+  const embedVideoLink = video1 + getVideoLink + video2;
+  embedVideo.value = embedVideoLink;
+});
+
+const copy = (link, btn) => {
+  link.select();
+  document.execCommand('copy');
+  btn.innerHTML = 'Copied!';
+  setTimeout(() => {
+    btn.innerHTML = 'Copy';
+  }, 2000);
+};
+
+copyBtn.addEventListener('click', () => {
+  return copy(directLink, copyBtn);
+});
+
+embedAudioCopyBtn.addEventListener('click', () => {
+  return copy(embedAudio, embedAudioCopyBtn);
+});
+embedVideoCopyBtn.addEventListener('click', () => {
+  return copy(embedVideo, embedVideoCopyBtn);
 });
