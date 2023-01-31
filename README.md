f786e37ab710a7d43d1bd088ad3b62f5e9942d34
sakilk130 <sakilk130@gmail.com>
2023-01-31 23:46:58 +0600

    feat: day-26 /  responsive-mobile-submenu

--- README.md
+++ README.md
@@ -101,3 +101,5 @@ This is a responsive mobile menu that is built with HTML, CSS and JavaScript. It
 ## Day 25 - Responsive Mobile Menu 2
 
 This is a responsive mobile menu with a hamburger icon. The menu is hidden by default and will be shown when the hamburger icon is clicked. The menu will be hidden again when the hamburger icon is clicked again.
+
+## Day 26 - Responsive Mobile Submenu
---
+++ day-26/responsive-mobile-submenu/app.js
@@ -0,0 +1,35 @@
+const links = document.querySelectorAll('a');
+const mobileMenu = document.querySelector('.fa-bars');
+const subMenu = document.querySelector('.three-sub');
+const back = document.querySelector('.back');
+
+const clickHandler = (e) => {
+  e.preventDefault();
+
+  const href = e.currentTarget.getAttribute('href');
+  const offsetTop = document.querySelector(href).offsetTop;
+  scroll({
+    top: offsetTop,
+    behavior: 'smooth',
+  });
+};
+
+for (const link of links) {
+  link.addEventListener('click', clickHandler);
+}
+
+mobileMenu.addEventListener('click', () => {
+  document.querySelector('.nav-wrapper').classList.toggle('show');
+  document.querySelector('.nav-links').classList.toggle('show');
+  mobileMenu.classList.toggle('fa-times');
+});
+
+subMenu.addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.remove('show');
+  document.querySelector('.submenu-three').classList.toggle('show');
+});
+
+back.addEventListener('click', () => {
+  document.querySelector('.nav-links').classList.toggle('show');
+  document.querySelector('.submenu-three').classList.remove('show');
+});
